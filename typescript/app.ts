55c97559491c8be1f52c8cbe2401fdf08e962530
tdunca <dunca.theresa@gmail.com>
2024-08-29 18:35:15 +0200

    Player, Board, App, Json
    Created player, created board, changed Index to app and json to make app work

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,6 +1,10 @@
 import { Game } from './game.js';
+import prompt from './helpers/prompt.js';
 
+const run = () => {
+  const name = prompt("Vad heter du? ")
+  const game = new Game(name);
+  game.start()
+}
 
-
-const game = new Game('PlayerOne', 'PlayerTwo');
-game.start();
+run()
--- typescript/game.ts
+++ typescript/game.ts
@@ -1,3 +1,16 @@
-export class Game {
+import { Player } from "./player.js";
 
+export class Game {
+	private playerX: Player
+  private playerO: Player
+
+  public constructor(playerXName: string) {
+    this.playerX = new Player(playerXName, "X")
+    this.playerO = new Player("Dumb bot", "O")
+  }
+
+  public start() {
+    console.log(`Hello from ${this.playerX.name} and ${this.playerO.name}`)
+  }
 }
+
